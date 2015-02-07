c571c6ab72bfda44e495578323969c5afa2d1f31
xaratustrah <shahab@zamail.de>
2015-02-08 00:20:37 +0100

    updates

--- pymtg.py
+++ pymtg.py
@@ -8,8 +8,9 @@ Feb 2015 Xaratustrah
 
 import json
 import os
-from numpy import genfromtxt
 import urllib.request as ur
+
+from numpy import genfromtxt
 
 
 class Card:
@@ -24,6 +25,15 @@ class Card:
 
     def __str__(self):
         return self.dict['name']
+
+    def get_types(self):
+        return self.dict['types']
+
+    def get_subtypes(self):
+        return self.dict['subtypes']
+
+    def get_watermark(self):
+        return self.dict['watermark']
 
     def get_mana_cost(self):
         return self.dict['manaCost']
@@ -59,6 +69,7 @@ class Card:
 class Deck:
     def __init__(self, name):
         self.card_list = []
+        self.nonland_card_list = []
         self.name = name
 
     def __str__(self):
@@ -78,6 +89,8 @@ class Deck:
             for j in range(cards_file[i][2]):
                 cc = Card(cards_file[i][0].decode("utf-8"), cards_file[i][1])
                 self.add_card(cc)
+        # self.unique_card_list = self.get_unique_list(self.card_list)
+        self.nonland_card_list = self.get_nonland()
 
     def add_card(self, card):
         self.card_list.append(card)
@@ -97,10 +110,144 @@ class Deck:
             for i in range(len(self.card_list)):
                 f.write('{}\t{}\t1\n'.format(self.card_list[i].block, self.card_list[i].number))
 
+                # TODO: change with generator function
+
+    def get_lands(self):
+        lst = []
+        for c in self.card_list:
+            if 'Land' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_sorceries(self):
+        lst = []
+        for c in self.card_list:
+            if 'Sorcery' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_instants(self):
+        lst = []
+        for c in self.card_list:
+            if 'Instant' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_enchantments(self):
+        lst = []
+        for c in self.card_list:
+            if 'Enchantment' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_planeswalkers(self):
+        lst = []
+        for c in self.card_list:
+            if 'Planeswalker' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_artifacts(self):
+        lst = []
+        for c in self.card_list:
+            if 'Artifact' in c.get_types():
+                lst.append(c)
+        return lst
+
+    def get_creatures(self):
+        lst = []
+        for c in self.card_list:
+            if 'Creature' in c.get_types():
+                lst.append(c)
+        return lst
+
+    # @staticmethod
+    def get_nonland(self):
+        lst = []
+        lst.extend(self.get_creatures())
+        lst.extend(self.get_artifacts())
+        lst.extend(self.get_enchantments())
+        lst.extend(self.get_planeswalkers())
+        lst.extend(self.get_sorceries())
+        lst.extend(self.get_instants())
+
+        return lst
+
+    @staticmethod
+    def print_list(lst):
+        for c in lst:
+            print(c)
+
+    @staticmethod
+    def get_unique_list(lst):
+        checked = []
+        lst_out = []
+        for c in lst:
+            if c.__str__() not in checked:
+                checked.append(c.__str__())
+                lst_out.append(c)
+        return lst_out
+
+    @staticmethod
+    def get_whites(lst):
+        checked = []
+        for c in lst:
+            if '{W}' in c.get_mana_cost():
+                checked.append(c)
+        return checked
+
+    @staticmethod
+    def get_blues(lst):
+        checked = []
+        for c in lst:
+            if '{U}' in c.get_mana_cost():
+                checked.append(c)
+        return checked
+
+    @staticmethod
+    def get_blacks(lst):
+        checked = []
+        for c in lst:
+            if '{B}' in c.get_mana_cost():
+                checked.append(c)
+        return checked
+
+    @staticmethod
+    def get_reds(lst):
+        checked = []
+        for c in lst:
+            if '{R}' in c.get_mana_cost():
+                checked.append(c)
+        return checked
+
+    @staticmethod
+    def get_greens(lst):
+        checked = []
+        for c in lst:
+            if '{G}' in c.get_mana_cost():
+                checked.append(c)
+        return checked
+
+    @staticmethod
+    def get_colorless(lst):
+        checked = []
+        for c in lst:
+            if '{W}' not in c.get_mana_cost():
+                if '{U}' not in c.get_mana_cost():
+                    if '{B}' not in c.get_mana_cost():
+                        if '{G}' not in c.get_mana_cost():
+                            if '{R}' not in c.get_mana_cost():
+                                checked.append(c)
+        return checked
+
+
+def make_all_cards():
+    allcards = Deck("allcards")
+    allcards.make_from_file('all_cards.txt')
+
 if __name__ == "__main__":
-    all_cards = Deck("all_cards")
-    all_cards.make_from_file('all_cards.txt')
-    print(all_cards)
-    print(all_cards.get_size())
-    # all_cards.download_images()
-    all_cards.save_to_disk()
+    allcards = Deck("allcards")
+    allcards.make_from_file('all_cards.txt')
+    lst = allcards.nonland_card_list
+    lst = Deck.get_unique_list(lst)
+    Deck.print_list(Deck.get_blacks(lst))
